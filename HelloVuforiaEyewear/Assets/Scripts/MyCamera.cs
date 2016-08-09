cc5dc59d84e6455ccb8e5ff43b27ae9f21b9848c
林暐傑 <peterlin424@gmail.com>
2016-08-09 17:28:22 +0800

    first

---
+++ HelloVuforiaEyewear/Assets/Scripts/MyCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCamera : MonoBehaviour {
+
+	Renderer rend;
+	void Start () {
+		rend = this.GetComponent<Renderer>();
+		WebCamTexture wc = new WebCamTexture ();
+		rend.material.mainTexture = wc;
+		wc.Play ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ HelloVuforiaEyewear/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		CameraDevice.Instance.SetFocusMode (CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
